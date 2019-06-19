b67dc1165459ef29fffffc6b917a181cc609e36c
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-19 19:56:01 +0000

    Initial commits of practicing compiling c files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - prints a string using printf.
+ *Description: (prints with proper grammar, but the outcome is a piece of art,)
+ *Return: 0 if completed successfully.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
