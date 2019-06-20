29f23bb56b9ac01c2fd775a05621d9c0d95fe2e5
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-20 20:29:35 +0000

    change from while loop to for loop

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,8 @@
 
 int main(void)
 {
-	int i = 0, num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-	while (num[i] > 0 && num[i] <= 9)
-		putchar('num[i]');
+	for (i = 0; i <= 9; i++)
+		putchar(i);
 	putchar ('\n');
 	return (0);
 }
