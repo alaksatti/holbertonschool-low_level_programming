767daaef7c3904d2f1af9776813f15ffae192fe2
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-21 06:06:48 +0000

    9 ammended

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,18 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i <= 9; ++i)
 	{
-		for (j = 0; j <= 9; ++j)
+		putchar(i + '0');
+		if (!(i == 9))
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-
-			if (!((i == 9) && (j == 9)))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
+
 	return (0);
 }
