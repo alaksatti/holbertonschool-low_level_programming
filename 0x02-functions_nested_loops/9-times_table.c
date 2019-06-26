e0d082ce36bad136b8c0bf03acf4ef82c721b55c
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-26 02:59:51 +0000

    deleted extra space before rows

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,17 +15,23 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			p = ((i * j));
-			if (p <= 9 && j != 9)
+			if (p <= 9 && j != 0 && j != 9)
 			{
 				_putchar(' ');
 				_putchar(p + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (p > 9 && j != 9)
+			if (p > 9 && j != 0 && j != 9)
 			{
 				_putchar((p / 10) + '0');
 				_putchar((p % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j ==0)
+			{
+				_putchar(p + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
