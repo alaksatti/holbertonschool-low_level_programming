0a29cdd04d4f923ab63e192ec31a8cac3ab9efe1
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-26 03:54:43 +0000

    changed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,45 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-			printf("%i, ", i);
-		printf(" ");
-
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
-			printf("%i, ", i);
-		printf(" ");
+		for (i = n; i >= 98; i--)
+		{
+			if (i <= 9)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 	}
+
+	if (n > 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 99)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i > 99)
+			{
+				_putchar(i / 100 + '0');
+				_putchar(i % 100 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
 }
