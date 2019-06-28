8fee5fa8f4cd4a36c9f0150d6bb975cd3617939e
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-28 02:42:30 +0000

    rearranged for loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,23 @@
 
 /**
  * more_numbers - prints 10 times the numbers 0 - 14/
- * Return: nothin.
+ * Return: nothing.
  */
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
+
 	for (i = 1; i <= 10; i++)
-		for (j = 0; j <= 14; j++)
 	{
-		if (j <= 9)
-			_putchar(i + '0');
-		else if (i > 9 && != 14)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar( i / 10 + '0');
-			_putchar( i % 10 + '0');
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
