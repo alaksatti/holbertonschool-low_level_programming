a9131b6c122776a687fa564853b9f14ad1aaca9a
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-28 02:58:15 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz buzz test. print 1 - 100. Prints Fizz, Buzz, Fizz Buzz for case.
+ *Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%i ", i);
+		printf("\n");
+
+
+		return (0);
+
+}
