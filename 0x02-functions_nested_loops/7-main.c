8f9c9d038b1f1a74be13d2f0b16ba6c45a63cb10
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-28 03:40:49 +0000

    new

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -9,12 +9,9 @@ int main(void)
 {
 	int r;
 
-	print_last_digit(105);
-	_putchar('\n');
-	print_last_digit(3);
-	_putchar('\n');
+	print_last_digit(98);
+	print_last_digit(0);
 	r = print_last_digit(-1024);
-	_putchar('\n');
 	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
