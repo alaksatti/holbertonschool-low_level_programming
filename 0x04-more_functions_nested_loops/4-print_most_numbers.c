099c806f2c0b949f892051274ac908486603aed9
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-28 00:33:13 +0000

    new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,11 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i != 2 && i != 4 && i <= 9)
+	while ( i <= 9)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i <= 4)
+			_putchar(i + '0');
+
 		i++;
 	}
 
