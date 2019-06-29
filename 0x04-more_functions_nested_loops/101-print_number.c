6bc3257fc07ad339992ba3ecdeedc515c764c298
Aalaa Satti <ala.k.satti@gmail.com>
2019-06-29 06:19:26 +0000

    initial

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints integers.
+ * @n: number to be checked
+ * Return: nothing
+ */
+
+
+void print_number(int n)
+{
+	int r;
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+
+	else if (n == 0)
+	{
+	        _putchar('0');
+		return;
+	}
+	else
+		m = n;
+
+	if (m > 0)
+	{
+		r = m % 10;
+		if ( m / 10 > 0)
+			print_number(m / 10);
+		_putchar(r + '0');
+	}
+}
