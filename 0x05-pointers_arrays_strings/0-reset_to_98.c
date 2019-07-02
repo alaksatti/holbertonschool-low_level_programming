81c073f4e1573c21effff333eb5804009021fca2
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-02 17:34:16 +0000

    Update value of n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+
+/**
+ * reset_to_98 - updates the value of a variable through a pointer.
+ * @n: pointer of type int to  used to update value.
+ * Return: nothing.
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+
+
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,21 @@
+#ifndef holberton_h
+#define holberton_h
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*holberton_h*/
