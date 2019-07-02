a152e7aea402a960c6948b226519336147f97d65
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-02 18:07:54 +0000

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print a string in reverse.
+ * @s: string to be reversed.
+ * Return: nothing.
+ */
+
+void print_rev(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = count - 1; i >= 0; i--)
+		_putchar(s[i]);
+}
