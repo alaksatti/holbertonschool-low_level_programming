6ac2714ca171f9f065f0d3b5d7aa9b5c547bc751
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-03 01:17:05 +0000

    fix indenting

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,9 @@ void puts_half(char *str)
 		count++;
 
 	if (count % 2  == 0)
-		for (i = (count) / 2; i <= count; ++i)
-			     _putchar(str[i]);
-     	else
+		for (i = count / 2; i <= count; ++i)
+			_putchar(str[i]);
+	else
 		for (i = ((count + 1) / 2); i <= count; ++i)
 			_putchar(str[i]);
 	_putchar('\n');
