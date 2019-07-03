cda28806bb974d1ffbb025a2f66b540d1d4543e1
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-03 04:19:02 +0000

    Condensed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%i, ", *(a + i));
-
-	printf("%i", *(a + (n - 1)));
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", *(a + i));
+		if (i < n - 1)
+			printf(", ");
+	}
 	putchar('\n');
 }
