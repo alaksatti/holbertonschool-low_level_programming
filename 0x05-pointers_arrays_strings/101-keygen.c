3d61d02fd3564186328533b4a349924b4ca1af4a
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-03 06:05:12 +0000

    password for crack me

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - generates random passwords for crackme.
+ *Return: 0.
+ */
+
+
+int main (void)
+{
+
+	srand(time(NULL));
+	printf("%i", rand);
+
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies string from one array into another.
+ * @dest: buffer to which the string will be copied into.
+ * @src: the string to be copied/
+ * Return: The buffer to which the string was copied into.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	for (i = 0; i != '\0'; i++)
+		*(dest + i) = *(str + i);
+	*(dest) = *strc
+	return (dest);
+
+
+}
