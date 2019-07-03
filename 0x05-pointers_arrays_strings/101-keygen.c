f38a8d6b7d02684cf14f1abaff5aadcbffec8101
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-03 07:05:30 +0000

    added possibilities

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,6 +10,10 @@
 
 int main(void)
 {
+	int i, r, sum;
+
+	char poss[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ0123456789";
+	char passwd[100];
 
 	srand(time(NULL));
 	sum = 0;
@@ -18,14 +22,14 @@ int main(void)
 	while (sum < (2772 - 122))
 	{
 	r = rand() % 62;
-	passwd[i] = a[r];
+	passwd[i] = poss[r];
 	sum+= passwd[i];
 	i++;
 	}
 
 	r = 2772 - sum;
 	passwd[i] = r;
-	printf("%i", passwd;
+	printf("%s", passwd);
 
 	return (0);
 }
