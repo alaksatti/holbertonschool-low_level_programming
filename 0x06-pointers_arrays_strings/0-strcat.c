1e0bb5fe10e79a11260806b454721c4ad537a088
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-06 19:35:22 +0000

    Betty compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,8 @@
 
 /**
  * _strcat - function that will concatenate two strings.
- * @dest - the string to which the next string will be concatenated to.
- * @src - the string that will be concatenated to the string dest.
+ * @dest: the string to which the next string will be concatenated to.
+ * @src: the string that will be concatenated to the string dest.
  * Return: Pointer to dest.
  */
 
@@ -13,6 +13,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
+
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
