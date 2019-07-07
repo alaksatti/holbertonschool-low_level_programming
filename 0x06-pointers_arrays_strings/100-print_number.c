c2d3bf6045538a603a90e3a95621997c7c6b6065
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-07 02:15:03 +0000

    initial commit of print numbers

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+
+/**
+ * print_number - prints an integer.
+ * @n - integer to be printed.
+ * Return: nothing.
+ */
+
+
+void print_number(int n)
+{
+	unsigned m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else
+		m = n;
+
+	if (m / 10)
+		print_number(m / 10);
+	_putchar(m % 10 + '0');
+}
