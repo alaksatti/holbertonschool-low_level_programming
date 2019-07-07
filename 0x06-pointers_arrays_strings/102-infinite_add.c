0f43ce9b48a7a064636cd5839744b5b8b73e884e
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-07 21:05:55 +0000

    cleaned up github

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,7 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
-
-
 /**
  * _strcpy - copies one string into another.
  * @dest: buffer to which string will be copied.
@@ -31,8 +28,13 @@ char *_strcpy(char *dest, char *src)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int leftover = 0, numin1, numin2, sumin = 0, i, j;
-	char t[99999999999];
+	int leftover = 0; /** number to be carried **/
+	int numin1; /**index of n1**/
+	int numin2; /**index of n2**/
+	int sumin = 0; /*index of temp[] **/
+	int i; /**count of n1**/
+	int j; /**count of n2**/
+	char temp[10000000]; /**temp array holding the result*/
 
 	for (i = 0; n1[i] != '\0'; i++)
 		n1[i] = n1[i] - '0';
@@ -44,35 +46,35 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 		for (; numin1 >= 0 && numin2 >= 0; numin1--, numin2--, ++sumin)
 		{
-			t[sumin] = (n1[numin1] + n2[numin2] + leftover) / 10;
-			leftover = (n1[numin1] + n2[numin2] + leftover) % 10;
+			temp[sumin] = (n1[numin1] + n2[numin2] + leftover) % 10;
+			leftover = (n1[numin1] + n2[numin2] + leftover) / 10;
 		}
 		if (numin1 == numin2 && leftover > 0)
-			t[sumin++] = leftover;
+			temp[sumin++] = leftover;
+
 		else if (numin1 > numin2)
 			while (numin1 >= 0)
 			{
-				t[sumin++] = (n1[numin1] + leftover) % 10;
+				temp[sumin++] = (n1[numin1] + leftover) % 10;
 				leftover = (n1[numin1] + leftover) / 10;
 				numin1--;
 			}
+
 		else if (numin2 > numin1)
 			while (numin2 >= 0)
 			{
-				t[sumin++] = (n2[numin2] + leftover) % 10;
+				temp[sumin++] = (n2[numin2] + leftover) % 10;
 				leftover = (n2[numin2] + leftover) / 10;
 				numin2--;
 			}
+
 		if (sumin + 1 > size_r - 1)
 			return (0);
-
-
 		else
 		{
-			for (i = 0; i <= sumin; i++)
-				t[i] = t[i] + '0';
-			t[i] = '\0';
-			_strcpy(t, r);
+			for (i = 0; i != '\0'; i++)
+				temp[i] = temp[i] + '0';
+			_strcpy(r, temp);
 			return (r);
 		}
 }
