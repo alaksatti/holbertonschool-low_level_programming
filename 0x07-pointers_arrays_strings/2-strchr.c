e13ba3e4df0372c10d8a9972eb93883af1e4e52f
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-09 20:31:41 +0000

    added null case

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
+
+
 /**
  * _strchr - locates a character in a string.
  * @s: pointer to string.
  * @c: character to be searched/
  * Return: Pointer to string.
  */
+
 char *_strchr(char *s, char c)
 {
 	int i, j;
@@ -12,15 +15,13 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		if (s[i] == c)
-			break;
+			return(s[i]);
 	}
 
-	if (s[i] != c)
-		return (0);
+	if (s[i] == '\0')
+		return('\0');
 
-	for (j = 0; s[j] != '\0'; j++, i++)
-		s[j] = s[i];
-	return (s);
+	return ('\0');
 
 
 
