10acb624a99ae7bbcf3083aa12867ed8afdb310b
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-09 20:40:00 +0000

    betty compliant

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		if (s[i] == c)
-			return(s[i]);
+			return (*(s + i));
 	}
 
 	if (s[i] == '\0')
-		return('\0');
+		return ('\0');
 
 	return ('\0');
 
