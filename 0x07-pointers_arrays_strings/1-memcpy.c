5687f9b68b001afe4c2630b99e3f48ba7e6a99fa
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-09 23:23:25 +0000

    Use pointer notation

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
+	for (i = 0; i < n; ++i)
+		*(dest + i) = *(src + i);
 
 	return (dest);
 }
