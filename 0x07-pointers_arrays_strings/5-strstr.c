840bcaa19349d4c5eb7ba3e265b606bc7f375bb8
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-10 03:45:27 +0000

    initial commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: string to be searched..
+ * @needle: string to be located.
+ * Return: Pointer to beginning of located substring.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	int i, j, k, count, start;
+
+	for (count = 0; needle[count] != '\0'; count++)
+		;
+
+
+
+	for (i = 0; haystack[i] != '\0'; i++)
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				start = i;
+
+				for (k = 0; k < count; k++)
+				{
+					if (haystack[i++] == needle[j++])
+						continue;
+					else
+						break;
+				}
+			}
+		}
+
+	if (haystack[i] == needle[j])
+		return (haystack + start);
+	return ('\0');
+
+
+
+}
