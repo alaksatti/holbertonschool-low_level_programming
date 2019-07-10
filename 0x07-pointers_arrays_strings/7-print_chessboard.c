4899122a50bd69cedd19c4aa5f940fbd790b0da3
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-10 05:01:36 +0000

    declare variables

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,6 +8,7 @@
 
 void print_chessboard(char (*a)[8])
 {
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
@@ -15,12 +16,5 @@ void print_chessboard(char (*a)[8])
 			_putchar(a[i][j]);
 		_putchar('\n');
 	}
-
-
-
-
-
-
-
 
 }
