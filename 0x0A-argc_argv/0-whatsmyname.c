349cd1d3febc67f6b6365b89dd340b5fcfefd563
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-14 23:15:43 +0000

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints name of program followed by new line.
+ * Return: 0 when completed successfuly.
+ */
+
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
