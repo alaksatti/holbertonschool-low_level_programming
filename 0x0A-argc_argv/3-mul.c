9873b48be8556f306148f32a28aa9b3a138b9cfa
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-15 00:15:21 +0000

    multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,44 @@
+#include "holberton.h"
+#include <limits.h>
+
+
+/**
+ *_atoi - converts a string to an integer, pulls integer and its sign.
+ * @s: string to be evaluated.
+ *Return: integer with its sign.
+ */
+
+
+
+int _atoi(char *s)
+{
+        unsigned int i, numfound = 0, negcounter = 0, finalint = 0;
+
+        for (i = 0; s[i] != '\0'; i++)
+        {
+	if (s[i] == '-')
+		negcounter++;
+
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+	finalint = finalint * 10 + (s[i] - '0');
+	numfound = 1;
+}
+	else if (numfound == 1)
+		break;
+}
+
+        if (negcounter % 2 == 0)
+                return (finalint);
+
+        else if (negcounter % 2 != 0)
+                return (-finalint);
+        else
+                return (0);
+}
+
 #include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - multiplies two numbers together.
@@ -9,9 +49,11 @@
 
 
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-	int i = argv[1] * argv[2];
+	int a = _atoi(argv[1]);
+	int b = _atoi(argv[2]);
+	int i = a * b;
 
 	if (argc == 3)
 		printf("%i\n", i);
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _atoi(char *s);
+
+
+
+#endif
