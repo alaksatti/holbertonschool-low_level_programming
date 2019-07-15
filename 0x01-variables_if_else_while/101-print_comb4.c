87c7a165c2149cca736dfaeee462af7cdddbce9e
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-15 21:28:08 +0000

    revised

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,31 +12,21 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 9; ++i)
-	{
-		for (j = j; j <= 9; ++j)
-		{
-			for (k = 0; k <= 9; ++k)
+	for (i = '0'; i <= '7'; ++i)
+		for (j = '1'; j <= '8'; ++j)
+			for (k = '2'; k <= '9'; ++k)
 			{
-				if ((i != j) && (j != k))
-				{
-					if ((i < j) && (j < k))
-					{
-						putchar(i);
-						putchar(j);
-						putchar(k);
+				putchar(i);
+				putchar(j);
+				putchar(k);
 
-						if (i == 7 && j == 8 && k == 9)
-							putchar('\n');
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+				if (!(i == '7' && j == '8' && k == '9'))
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
-		}
-	}
+	putchar ('\n');
+
 	return (0);
 }
