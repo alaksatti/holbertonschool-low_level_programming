4f8776d5c29d7d87f491251cd561ba7eaca012c5
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-15 00:19:16 +0000

    betty compliant

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,29 +12,27 @@
 
 int _atoi(char *s)
 {
-        unsigned int i, numfound = 0, negcounter = 0, finalint = 0;
+	unsigned int i, numfound = 0, negcounter = 0, finalint = 0;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-	if (s[i] == '-')
-		negcounter++;
-
-	else if (s[i] >= '0' && s[i] <= '9')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	finalint = finalint * 10 + (s[i] - '0');
-	numfound = 1;
-}
-	else if (numfound == 1)
-		break;
-}
+		if (s[i] == '-')
+			negcounter++;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			finalint = finalint * 10 + (s[i] - '0');
+			numfound = 1;
+		}
+		else if (numfound == 1)
+			break;
+	}
+	if (negcounter % 2 == 0)
+		return (finalint);
 
-        if (negcounter % 2 == 0)
-                return (finalint);
-
-        else if (negcounter % 2 != 0)
-                return (-finalint);
-        else
-                return (0);
+	else if (negcounter % 2 != 0)
+		return (-finalint);
+	else
+		return (0);
 }
 
 #include <stdio.h>
