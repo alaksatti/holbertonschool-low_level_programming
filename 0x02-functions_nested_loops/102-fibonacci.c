c8c602e9cb229946f02f96e7d21ebdc0bc9174f0
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-16 04:35:04 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints first 50 fib numbers.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long fib[50];
+	int i;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i = 2; i < 50; ++i)
+		fib[i] = (fib[i - 2] + fib[i - 1]);
+
+	for (i = 1; i < 49; ++i)
+		printf("%li, ", fib[i]);
+
+	printf("%li\n", fib[49]);
+
+	return (0);
+}
