5e443c84efb77863b458a28f691c759af7c349ee
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-16 21:55:37 +0000

    added empty string if s1 or s2 is null)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,13 @@ char *str_concat(char *s1, char *s2)
 	char *arr = NULL;
 	unsigned int i, j;
 
+
+	if (s1 == NULL)
+		sl = "";
+
+	if (s2 == NULL)
+		s2 == "";
+
 	for (i = 0; s1[i] != '\0'; ++i)
 		;
 	for (j = 0; s2[j] != '\0'; ++j)
