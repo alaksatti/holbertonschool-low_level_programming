54106203b14c2972351b202189849ddbada0e451
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-16 03:21:03 +0000

    betty compliant

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
 
-
 /**
  * print_times_table - prints the times table of value n up to 15.
  * @n: value of times table.
  * Return: 0 upon successful completion.
  */
-
 
 void print_times_table(int n)
 {
@@ -14,37 +12,38 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
+
 	for (i = 0; i <= n; ++i)
 		for (j = 0; j <= n; ++j)
-			{
-				p = i * j;
-				if (j == 0)
-					_putchar('0');
+		{
+			p = i * j;
 
-				else if (p > 9 && p < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p / 10 + '0');
-					_putchar(p % 10 + '0');
-				}
-				else if (p >= 100)
-				{
-					_putchar(' ');
-					_putchar(p / 100 + '0');
-					_putchar((p / 10) % 10 + '0');
-					_putchar(p % 10 + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p + '0');
-				}
-				if (!(j == n))
-					_putchar(',');
-				else
-					_putchar('\n');
+			if (j == 0)
+				_putchar('\n');
+			else if (p > 9 && p < 100)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(p / 10 + '0');
+				_putchar(p % 10 + '0');
 			}
+			else if (p >= 100)
+			{
+				_putchar(' ');
+				_putchar(p / 100 + '0');
+				_putchar((p / 10) % 10 + '0');
+				_putchar(p % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(p + '0');
+			}
+			if (!(j == n))
+				_putchar(p + '0');
+			else
+				_puthcar('\n');
+		}
 }
