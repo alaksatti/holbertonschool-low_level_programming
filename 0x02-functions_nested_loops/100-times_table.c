01c7593c40a5f432b454317988171e0506a4a03b
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-16 03:08:23 +0000

    betty compliant

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,16 +14,12 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
-	else
-		for (i = 0; i <= n; ++i)
-			for (j = 0; j <= n; ++j)
+	for (i = 0; i <= n; ++i)
+		for (j = 0; j <= n; ++j)
 			{
 				p = i * j;
-
 				if (j == 0)
-				{
 					_putchar('0');
-				}
 
 				else if (p > 9 && p < 100)
 				{
