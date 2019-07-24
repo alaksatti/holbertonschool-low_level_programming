1fb0a912ee494b08a8e7cfbb82ac6b7b908ce162
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-24 21:27:45 +0000

    Remove extra spaces

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints opcde for main function.
+ * @argc: number of arguments/
+ * @argv: arguments passed.
+ * Return: 0 if successsful otherwise print opcode in hex
+ */
 int main(int argc, char **argv)
 {
 	int opcode, i;
@@ -16,12 +22,14 @@ int main(int argc, char **argv)
 	if (opcode < 0)
 	{
 		printf("Error\n");
-		exit (2);
+		exit(2);
 	}
 	for (i = 0; i < opcode; i++)
 	{
-		printf("%2x", *((unsigned char *)main + i));
-		putchar(' ');
+		printf("%02x", *((unsigned char *)main + i));
+
+		if (i < (opcode - 1))
+			putchar(' ');
 	}
 	printf("\n");
 
