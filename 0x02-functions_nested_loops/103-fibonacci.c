6545e10cd0ac6682d8e758dd888dec58c9af7358
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-25 06:14:46 +0000

    move file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-
 /**
- * main - prints the even terms of fib numbers whose values are below 4 million.
+ * main - program that finds and prints the sum of the even-valued terms.
  * Return: 0;
  */
 
 
 int main(void)
 {
-	long i, fib[50], sum = 0;
 
-	fib[0] = 1;
-	fib[1] = 2;
+	long int sum = 2;
 
-
-	for (i = 2; fib[i] <= 4,000,000; ++i)
-	{
-		if (i % 2 == 0)
+	for (i = 3; i < 4000000; i++)
+		if ((i % 2) == 0)
 			sum += i;
-		fib[i] = fib[i - 2] + fib [i - 1];
-	}
 
 	printf("%li\n", sum);
-
 	return (0);
+
+
+
+
 }
