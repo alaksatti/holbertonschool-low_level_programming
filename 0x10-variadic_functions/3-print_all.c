b686ff2a00065037f04a271fd6364504b6c3ea98
Aalaa Satti <ala.k.satti@gmail.com>
2019-07-27 19:55:26 +0000

    add condition to if statement

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -45,9 +45,10 @@ void print_all(const char * const format, ...)
 		}
 
 	if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
-	     || format[i] == 's'))
+	     || format[i] == 's') && format[i + 1] != '\0' )
 		printf(", ");
 	i++;
 	}
 	printf("\n");
+	va_end(fmt);
 }
