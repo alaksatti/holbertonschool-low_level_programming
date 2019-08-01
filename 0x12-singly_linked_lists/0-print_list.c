df0e86d228e857ef1f90a5e2b1acdda6f8ff9e85
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-01 23:48:29 +0000

    initial commits

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+
+
+/**
+ * print_list - prints all elements of linked list of type list_t.
+ * @h: linked list of type list_t.
+ * Return: number of notes in size_t.
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	size_t num = 0;
+
+	while (h)
+	{
+		if (h -> str)
+			printf("[%u] %s\n", h -> len, h -> str);
+		else
+			printf("[0] (nil)\n");
+		num++;
+
+		h = h -> next;
+
+	}
+
+
+	return (num);
+
+}
