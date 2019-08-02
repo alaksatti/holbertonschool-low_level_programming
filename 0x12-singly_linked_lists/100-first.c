39f44aea5d3c537c9c265dcd17102d80ed7385c4
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-02 03:54:02 +0000

    intiial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+void printf(void) __attribute__((constructor));
+
+/**
+ * print - prints a message before main is executed.
+ * Return: nothing.
+ */
+
+
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n\
+I bore my house upon my back!\n");
+
+}
