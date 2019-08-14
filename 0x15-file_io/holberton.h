97a878841e4b78d3da47aee990846a134543e5a2
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-14 05:40:44 +0000

    Add more prototypes

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -12,5 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_file_descriptor(int fd1, int fd2);
-
+void write_error(char *arg);
+void read_error(char *arg);
+void arg_error(void);
+void closing_error(int fd);
 #endif
