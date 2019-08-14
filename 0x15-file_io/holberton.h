d5938c0f612174919caf8affb5a80ba506098203
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-14 02:51:20 +0000

    modified

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,13 +5,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int main(int argc, char **argv);
 void close_file_descriptor(int fd1, int fd2);
 
 #endif
