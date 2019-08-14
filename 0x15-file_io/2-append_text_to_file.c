ffd9bbac558328b1f610fd5c4742ec698654669a
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-14 02:33:47 +0000

    rearrange

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,10 +32,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	wf = write(fd, text_content, i);
 
-	close(fd);
-
 	if (wf < i)
+	{
+		close(fd);
 		return (-1);
+	}
 
+	close(fd);
 	return (1);
 }
