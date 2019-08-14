6a56f08be89855f1e816e0c9f6412dcca64e8c9f
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-14 01:53:49 +0000

    betty compliant and fix variable name

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,8 +2,8 @@
 
 /**
  * create_file -  function that creates a file.
- * @filename - filename.
- * @text_content - string to write to the file.
+ * @filename: filename.
+ * @text_content: string to write to the file.
  * Return: 1 on success.
  */
 
@@ -15,7 +15,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
 
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 
@@ -31,11 +30,11 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
-	wf = write(fd, text_content, len);
+	wf = write(fd, text_content, i);
 
 	if (wf != i)
 	{
-		close (fd);
+		close(fd);
 		return (-1);
 	}
 
