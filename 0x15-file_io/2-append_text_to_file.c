42412ecff46bb957f561fe3f6de3b66e7f5d5772
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-14 02:29:09 +0000

    made more concise

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,15 +30,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
-
 	wf = write(fd, text_content, i);
 
-	if (wf < i)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	close(fd);
+
+	if (wf < i)
+		return (-1);
+
 	return (1);
 }
