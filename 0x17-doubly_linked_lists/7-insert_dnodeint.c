a89558c09b523c578454c8d0c0677b4207b72972
Aalaa Satti <ala.k.satti@gmail.com>
2019-09-16 06:53:35 +0000

    revise

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,35 +17,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	newnode = malloc(sizeof(dlistint_t));
 
-	if (newnode)
+	if (!newnode)
+		return (NULL);
+
+	newnode->n = n;
+
+	if (!idx)
 	{
-		newnode->n = n;
-
-		if (!idx)
-		{
-			newnode->prev = NULL;
-			newnode->next = *h;
-			*h = newnode;
-			return (newnode);
-		}
-
-		while (nodescanner && idx > 1 && nodescanner->next)
-		{
-			nodescanner = nodescanner->next;
-			idx--;
-		}
-
-		if (!nodescanner || idx > 1)
-			return (NULL);
-
-		newnode->next = nodescanner->next;
-		newnode->prev = nodescanner;
-
-		if (nodescanner->next)
-			nodescanner->next->prev = newnode;
-
-		nodescanner->next = newnode;
+		newnode->prev = NULL;
+		newnode->next = *h;
+		*h = newnode;
+		return (newnode);
 	}
+
+	while (nodescanner && idx > 1 && nodescanner->next)
+	{
+		idx--;
+		nodescanner = nodescanner->next;
+	}
+
+	if (!nodescanner || idx > 1)
+		return (NULL);
+
+	newnode->next = nodescanner->next;
+	newnode->prev = nodescanner;
+
+	if (nodescanner->next)
+		nodescanner->next->prev = newnode;
+
+	nodescanner->next = newnode;
 
 	return (newnode);
 }
