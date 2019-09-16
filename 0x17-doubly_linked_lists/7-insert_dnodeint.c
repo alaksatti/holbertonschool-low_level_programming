8add73d0fdf1d030e1eaec3bd7c1dccfd1f8b85e
Aalaa Satti <ala.k.satti@gmail.com>
2019-09-16 06:38:27 +0000

    change size for malloc

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 
-	newnode = malloc(sizeof(newnode));
+	newnode = malloc(sizeof(dlistint_t));
 
 	if (newnode)
 	{
