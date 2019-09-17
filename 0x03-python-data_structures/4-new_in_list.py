7b9c5509fd3b24fc38cf3654d25f9908058dc485
Aalaa Satti <ala.k.satti@gmail.com>
2019-09-17 08:56:49 +0000

    initial commits

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+
+    length = len(my_list)
+
+    new_list = my_list.copy()
+
+    if idx < 0 or idx > length:
+        return new_list
+
+    new_list.insert(idx, element)
+
+    return new_list
