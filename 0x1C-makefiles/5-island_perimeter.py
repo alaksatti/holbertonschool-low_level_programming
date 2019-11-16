a2de7279ac0239dddd59f40eed3644cfa4dd9968
Aalaa Satti <ala.k.satti@gmail.com>
2019-11-16 00:55:34 +0000

    initial commits

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" finding the perimeter of an island"""
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island"""
+    for row in grid:
+        row.insert(0,0)
+        row.append(0)
+    columns = len(grid[0])
+    grid.insert(0, [0] * columns)
+    grid.append([0] * columns)
+    rows = len(grid)
+    perimeter = 0
+
+
+
+    return perimeter
