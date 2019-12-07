cec8d6e2cd0d75157e9fc9f8e871694870969c70
Aalaa Satti <ala.k.satti@gmail.com>
2019-12-07 02:33:19 +0000

    add case for if parent is null

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -13,9 +13,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	binary_tree_t *right_node;
 
+	if (!parent)
+		return (NULL);
 
 	right_node = binary_tree_node(parent, value);
-
 
 	if (!right_node)
 		return (NULL);
